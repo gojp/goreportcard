37ecf5912c2d017249cff82764c03c292a1ab25c
Shawn Smith <shawnpsmith@gmail.com>
2015-02-03 19:13:14 +0900

    add 120 to timeout

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func clone(url string) error {
 	dir := dirName(url)
 	_, err := os.Stat(dir)
 	if os.IsNotExist(err) {
-		cmd := exec.Command("timeout", "git", "clone", "--depth", "1", "--single-branch", url, dir)
+		cmd := exec.Command("timeout", "120", "git", "clone", "--depth", "1", "--single-branch", url, dir)
 		if err := cmd.Run(); err != nil {
 			return fmt.Errorf("could not run git clone: %v", err)
 		}
