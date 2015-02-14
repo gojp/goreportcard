5927d9d69a6dd685c8bfaf66e9e87173704c86d8
shawnps <shawnpsmith@gmail.com>
2015-02-13 21:44:50 -0800

    add total repo count to high scores/stats page

--- handlers/high_scores.go
+++ handlers/high_scores.go
@@ -13,13 +13,13 @@ func add(x, y int) int {
 	return x + y
 }
 
-var highScores []struct {
-	Repo    string
-	Files   int
-	Average float64
-}
-
 func HighScoresHandler(w http.ResponseWriter, r *http.Request) {
+	var highScores []struct {
+		Repo    string
+		Files   int
+		Average float64
+	}
+
 	db := db.Mongo{URL: mongoURL, Database: mongoDatabase, CollectionName: mongoCollection}
 	coll, err := db.Collection()
 	if err != nil {
@@ -35,8 +35,15 @@ func HighScoresHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	count, err := coll.Count()
+	if err != nil {
+		log.Println("ERROR: could not get high scores: ", err)
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
 	funcs := template.FuncMap{"add": add}
 	t := template.Must(template.New("high_scores.html").Funcs(funcs).ParseFiles("templates/high_scores.html"))
 
-	t.Execute(w, map[string]interface{}{"HighScores": highScores})
+	t.Execute(w, map[string]interface{}{"HighScores": highScores, "Count": count})
 }
