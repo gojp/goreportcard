76876474262dd22caf80ee8be69095ae450cf3a8
Shawn Smith <shawnpsmith@gmail.com>
2015-03-24 22:40:39 +0900

    format scores more nicely

--- handlers/high_scores.go
+++ handlers/high_scores.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"text/template"
@@ -13,14 +14,12 @@ func add(x, y int) int {
 	return x + y
 }
 
+func formatScore(x float64) string {
+	return fmt.Sprintf("%.2f", x*100)
+}
+
 // HighScoresHandler handles the stats page
 func HighScoresHandler(w http.ResponseWriter, r *http.Request) {
-	var highScores []struct {
-		Repo    string
-		Files   int
-		Average float64
-	}
-
 	session, err := mgo.Dial(mongoURL)
 	if err != nil {
 		log.Println("ERROR: could not get collection:", err)
@@ -30,6 +29,11 @@ func HighScoresHandler(w http.ResponseWriter, r *http.Request) {
 	defer session.Close()
 	coll := session.DB(mongoDatabase).C(mongoCollection)
 
+	var highScores []struct {
+		Repo    string
+		Files   int
+		Average float64
+	}
 	err = coll.Find(bson.M{"files": bson.M{"$gt": 100}}).Sort("-average").All(&highScores)
 	if err != nil {
 		log.Println("ERROR: could not get high scores: ", err)
@@ -39,12 +43,12 @@ func HighScoresHandler(w http.ResponseWriter, r *http.Request) {
 
 	count, err := coll.Count()
 	if err != nil {
-		log.Println("ERROR: could not get high scores: ", err)
+		log.Println("ERROR: could not get count of high scores: ", err)
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	funcs := template.FuncMap{"add": add}
+	funcs := template.FuncMap{"add": add, "formatScore": formatScore}
 	t := template.Must(template.New("high_scores.html").Funcs(funcs).ParseFiles("templates/high_scores.html"))
 
 	t.Execute(w, map[string]interface{}{"HighScores": highScores, "Count": count})
