11d5c1db38056130bfcb55ff78054ae85da3a473
Shawn Smith <shawnpsmith@gmail.com>
2015-08-11 17:50:07 +0900

    remove shutdown notice, add digital ocean attribution logo/link

--- main.go
+++ main.go
@@ -33,7 +33,6 @@ func main() {
 	http.HandleFunc("/report/", makeHandler("report", handlers.ReportHandler))
 	http.HandleFunc("/badge/", makeHandler("badge", handlers.BadgeHandler))
 	http.HandleFunc("/high_scores/", handlers.HighScoresHandler)
-	http.HandleFunc("/shutdown/", handlers.ShutdownHandler)
 	http.HandleFunc("/", handlers.HomeHandler)
 
 	fmt.Println("Running on 127.0.01:8080...")
