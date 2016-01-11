a52b388715ad04c5083cb55c3886ef7112036112
Shawn Smith <shawnpsmith@gmail.com>
2016-01-11 12:47:19 +0900

    add cyclo-over flag

--- check/gocyclo.go
+++ check/gocyclo.go
@@ -18,7 +18,7 @@ func (g GoCyclo) Weight() float64 {
 
 // Percentage returns the percentage of .go files that pass gofmt
 func (g GoCyclo) Percentage() (float64, []FileSummary, error) {
-	return GoTool(g.Dir, g.Filenames, []string{"gometalinter", "--disable-all", "--enable=gocyclo"})
+	return GoTool(g.Dir, g.Filenames, []string{"gometalinter", "--disable-all", "--enable=gocyclo", "--cyclo-over=15"})
 }
 
 // Description returns the description of GoCyclo
