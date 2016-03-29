4cbafefc19c745049307fdc4ff06e600a302b3d4
Andrew Schwartz <andrew@compose.io>
2016-03-30 01:12:10 +0900

    sort checks by name

--- handlers/checks.go
+++ handlers/checks.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 
@@ -195,9 +196,17 @@ func newChecksResp(repo string, forceRefresh bool) (checksResp, error) {
 		}
 	}
 
+	sort.Sort(ByName(resp.Checks))
 	resp.Average = total
 	resp.Issues = len(issues)
 	resp.Grade = grade(total * 100)
 
 	return resp, nil
 }
+
+// ByName implements sorting for checks alphabetically by name
+type ByName []score
+
+func (a ByName) Len() int           { return len(a) }
+func (a ByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
