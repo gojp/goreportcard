55ed1589484faec00a3cc9ca72fcd680ed0eff94
Herman Schaaf <hermanschaaf@gmail.com>
2016-05-30 21:23:58 +0800

    Custom 404 page
    Closes #101

---
+++ handlers/error.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		t := template.Must(template.New("404.html").ParseFiles("templates/404.html"))
+		t.Execute(w, nil)
+	}
+}
