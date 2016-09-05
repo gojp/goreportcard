05dd6324e71ecd94e51133b2d0a1479a4fb71629
Shawn Smith <shawnpsmith@gmail.com>
2016-09-06 00:35:46 +0900

    add method for checking gofmt using go/format package (not currently switched on)

--- check/gofmt.go
+++ check/gofmt.go
@@ -19,6 +19,7 @@ func (g GoFmt) Weight() float64 {
 // Percentage returns the percentage of .go files that pass gofmt
 func (g GoFmt) Percentage() (float64, []FileSummary, error) {
 	return GoTool(g.Dir, g.Filenames, []string{"gometalinter", "--deadline=180s", "--disable-all", "--enable=gofmt"})
+	// return GoFmtNative(g.Dir, g.Filenames)
 }
 
 // Description returns the description of gofmt
