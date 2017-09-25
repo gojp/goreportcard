3d9a86010aec6b7798a602f20ee066f8b492c982
Herman Schaaf <hermanschaaf@gmail.com>
2017-09-25 21:41:09 +0100

    Add Prometheus monitoring page under /metrics

--- handlers/about.go
+++ handlers/about.go
@@ -1,15 +1,12 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 	"text/template"
 )
 
 // AboutHandler handles the about page
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving about page")
-
 	t := template.Must(template.New("about.html").Delims("[[", "]]").ParseFiles("templates/about.html", "templates/footer.html"))
 	t.Execute(w, map[string]interface{}{
 		"google_analytics_key": googleAnalyticsKey,
--- handlers/assets.go
+++ handlers/assets.go
@@ -1,19 +1,15 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 )
 
 // AssetsHandler handles serving static files
 func AssetsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving " + r.URL.Path[1:])
-
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
 // FaviconHandler handles serving the favicon.ico
 func FaviconHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving " + r.URL.Path[1:])
 	http.ServeFile(w, r, "assets/favicon.ico")
 }
