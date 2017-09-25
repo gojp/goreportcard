572182cf9d58c549ee9d532f31c6005e6ce3f96a
Herman Schaaf <hermanschaaf@gmail.com>
2017-09-25 21:41:09 +0100

    Add Prometheus monitoring page under /metrics

--- handlers/assets.go
+++ handlers/assets.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 )
 
 // AssetsHandler handles serving static files
 func AssetsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving " + r.URL.Path[1:])
 	w.Header().Set("Cache-Control", "max-age=86400")
 
 	http.ServeFile(w, r, r.URL.Path[1:])
@@ -15,6 +13,5 @@ func AssetsHandler(w http.ResponseWriter, r *http.Request) {
 
 // FaviconHandler handles serving the favicon.ico
 func FaviconHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving " + r.URL.Path[1:])
 	http.ServeFile(w, r, "assets/favicon.ico")
 }
