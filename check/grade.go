c958ecb465aba5ef3d7ba3ed32b0318c660f307c
Jeremy Rans <jrans@vendasta.com>
2018-12-14 23:35:02 -0600

    refactor to prepare for CLI

---
+++ check/grade.go
@@ -0,0 +1,37 @@
+package check
+
+// Grade represents a grade returned by the server, which is normally
+// somewhere between A+ (highest) and F (lowest).
+type Grade string
+
+// The Grade constants below indicate the current available
+// grades.
+const (
+	GradeAPlus Grade = "A+"
+	GradeA           = "A"
+	GradeB           = "B"
+	GradeC           = "C"
+	GradeD           = "D"
+	GradeE           = "E"
+	GradeF           = "F"
+)
+
+// GradeFromPercentage is a helper for getting the GradeFromPercentage for a percentage
+func GradeFromPercentage(percentage float64) Grade {
+	switch {
+	case percentage > 90:
+		return GradeAPlus
+	case percentage > 80:
+		return GradeA
+	case percentage > 70:
+		return GradeB
+	case percentage > 60:
+		return GradeC
+	case percentage > 50:
+		return GradeD
+	case percentage > 40:
+		return GradeE
+	default:
+		return GradeF
+	}
+}
--- handlers/badge.go
+++ handlers/badge.go
@@ -2,48 +2,13 @@ package handlers
 
 import (
 	"fmt"
+	"github.com/gojp/goreportcard/check"
 	"log"
 	"net/http"
 	"strings"
 )
 
-// Grade represents a grade returned by the server, which is normally
-// somewhere between A+ (highest) and F (lowest).
-type Grade string
-
-// The Grade constants below indicate the current available
-// grades.
-const (
-	GradeAPlus Grade = "A+"
-	GradeA           = "A"
-	GradeB           = "B"
-	GradeC           = "C"
-	GradeD           = "D"
-	GradeE           = "E"
-	GradeF           = "F"
-)
-
-// grade is a helper for getting the grade for a percentage
-func grade(percentage float64) Grade {
-	switch {
-	case percentage > 90:
-		return GradeAPlus
-	case percentage > 80:
-		return GradeA
-	case percentage > 70:
-		return GradeB
-	case percentage > 60:
-		return GradeC
-	case percentage > 50:
-		return GradeD
-	case percentage > 40:
-		return GradeE
-	default:
-		return GradeF
-	}
-}
-
-func badgePath(grade Grade, style string, dev bool) string {
+func badgePath(grade check.Grade, style string, dev bool) string {
 	return fmt.Sprintf("assets/badges/%s_%s.svg", strings.ToLower(string(grade)), strings.ToLower(style))
 }
 
