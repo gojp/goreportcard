a40e0b584bfc6d23a2dd0381cf639fcb7f3516ff
Shawn Smith <shawnpsmith@gmail.com>
2021-11-28 12:07:38 +0900

    update install script

--- scripts/make-install.sh
+++ scripts/make-install.sh
@@ -3,6 +3,6 @@
 go install ./vendor/github.com/alecthomas/gometalinter
 
 go install ./vendor/golang.org/x/lint/golint
-go install ./vendor/github.com/fzipp/gocyclo
+go install ./vendor/github.com/fzipp/gocyclo/cmd/gocyclo
 go install ./vendor/github.com/gordonklaus/ineffassign
 go install ./vendor/github.com/client9/misspell
