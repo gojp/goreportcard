4ba4def621ad99de6f744b397eed94e19826642c
Que Yanghua <yanghua.que@olaola.chat>
2022-01-30 02:57:49 +0800

    fix installation of misspell

--- scripts/make-install.sh
+++ scripts/make-install.sh
@@ -5,4 +5,4 @@ go install ./vendor/github.com/alecthomas/gometalinter
 go install ./vendor/golang.org/x/lint/golint
 go install ./vendor/github.com/fzipp/gocyclo/cmd/gocyclo
 go install ./vendor/github.com/gordonklaus/ineffassign
-go install ./vendor/github.com/client9/misspell
+go install ./vendor/github.com/client9/misspell/cmd/misspell
